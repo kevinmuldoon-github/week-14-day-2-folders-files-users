e744baee01101d52a8354de4f2bc2a053231e820
Kevin Muldoon <100124261+kevinmuldoon-github@users.noreply.github.com>
2022-05-24 15:59:24 +0100

    Created file structure using Spring

---
+++ src/main/java/com/codeclan/example/foldersfilesusers/FoldersFilesUsersApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.foldersfilesusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoldersFilesUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoldersFilesUsersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeclan/example/foldersfilesusers/FoldersFilesUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.foldersfilesusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoldersFilesUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
